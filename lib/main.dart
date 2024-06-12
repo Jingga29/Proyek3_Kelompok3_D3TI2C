c5b0fe3e5f69376e00889a73aeac9b05aeb97e0b
jingga29 <jinggajoshiorlandoii@gmail.com>
2024-06-12 20:25:00 +0700

    landingpage

---
+++ lib/main.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+//import 'login.dart'; // Import login page
+//import 'signup.dart'; // Import sign up page
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+      routes: {
+        //'/login': (context) => LoginPage(), 
+        //'/signup': (context) => SignUpPage(), 
+      },
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF5D13E7),
+      body: Padding(
+        padding: const EdgeInsets.only(
+          top: 30,
+          right: 10,
+          left: 10,
+          bottom: 30,
+        ),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(height: MediaQuery.of(context).size.height * 0.1),
+                RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: "Mata",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      TextSpan(
+                        text: "Fish!",
+                        style: TextStyle(
+                          color: Colors.yellow,
+                          fontSize: 45,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 20), // Add some space between the RichText and the image
+                Image.asset(
+                  'assets/images/leleellips.png',
+                  height: 450,
+                ),
+                SizedBox(height: 20), // Add some space between the image and the text
+                Text(
+                  "Get Started",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        // Aksi ketika tombol Sign Up ditekan
+                        Navigator.pushNamed(context, '/signup'); // Navigasi ke halaman sign up
+                      },
+                      child: Text(
+                        'Sign Up',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/login'); // Navigasi ke halaman login
+                      },
+                      child: Text(
+                        'Log In',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.yellow,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
